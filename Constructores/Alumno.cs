638cdbf219d8d45de60d2592b4fa3dd0fe7fcb89
moises orellana <moorellana@unah.hn>
2020-10-25 08:41:33 -0600

    Constructores-V11

---
+++ Constructores/Alumno.cs
@@ -0,0 +1,20 @@
+public class Alumno
+{
+    public double id { get; set;}
+
+    public string nombre { get; set;}
+
+    public string apellido { get; set;}
+
+
+    public Alumno()
+    {
+        id = 0405199500389;
+    }
+
+
+    public Alumno(double i)
+    {
+        id = i;
+    }
+}
---
+++ Constructores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.id = 0405199500389;
+            a.nombre = "Moises";
+            a.apellido = "Orellana";
+
+            Alumno b = new Alumno();
+            a.id = 0405199600398;
+            a.nombre = "Carlos";
+            a.apellido = "Galamez";
+
+            Alumno c = new Alumno(0402189700458);
+            b.nombre = "Oscar";
+            b.apellido = "Melgar";
+
+            Console.WriteLine(a.id);
+            Console.WriteLine(b.id);
+            Console.WriteLine(c.id);
+        }
+    }
+}
